fc7988a43c1683567ca9fdb89fc8087c4d798620
kimkundad <kim.kundad@gmail.com>
2021-11-17 16:04:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+const https = require('https');
+const crypto = require("crypto");
+
+
+var cron = require('node-cron');
+cron.schedule('*/1 * * * *', function(){
+   console.log('running a task every two minutes');
+
+   var n = crypto.randomInt(0, 1000000);
+
+   https.get('https://xn--888-4nlyax.com/include/auto_bank.php?v'+n, (res) => {
+    let rawHtml = '';
+    res.on('data', (chunk) => { rawHtml += chunk; });
+    res.on('end', () => {
+        try {
+            console.log(rawHtml);
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+});
+
+
+
+});
+
